96d9783b6ec3f4f8e211b1cf3284b31f35a8e729
Rahul M <rahul@cubixcorps-Mac-mini.local>
2025-08-04 19:35:34 +0530

    feat: 🎸 Added a new files

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+function speak() {
+  return `Hello, ${this.name}`;
+}
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const user = {
+  name: "Bob",
+  greet: speak
+};
+
+console.log(user.greet()); // "Hello, Bob"
